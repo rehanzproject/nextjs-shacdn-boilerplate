4191c43519015bbc459b9b872bb45f2fb32202fa
rehanzproject <rehanmaul111@gmail.com>
2024-09-26 07:04:04 +0700

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/ModeToggle";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <ModeToggle />
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
@@ -47,6 +50,9 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <Button variant='secondary'>
+            Hello 
+          </Button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
